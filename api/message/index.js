a73a5acc7edb33e5f669c92f1693da2ae1ac857b
Anthony Castaneda <anthony.castaneda@gmail.com>
2023-01-03 06:22:31 -0600

    update

--- api/message/index.js
+++ api/message/index.js
@@ -1,15 +1,18 @@
 //const https = require('https');
 const quotes = [
-    'Break what must be broken, once for all, that\'s all, and take the suffering on oneself.',
+    'You never stop, never get to rest.',
     'We do not suffer by accident.',
     'Grief is forever. It doesn\'t go away; it becomes a part of you, step for step, breath for breath.',
     'My body kills me, so I kill it.',
-    'Her absence is like the sky, spread over everything..',
+    'Her absence is like the sky, spread over everything.',
     'Death is only the end if you assume the story is about you.',
     'It is possible for things to get worse without limit.',
     'The sadness will last forever.',
     'I\'m hoping he can save me, even though I realize he can\'t.',
-    'She is never coming back.'
+    'She is never coming back.',
+    'Time that withers you will wither me.',
+    'They give birth astride of a grave, the light gleams an instant, then it\'s night once more.',
+    'Absence is a house so vast that inside you will pass through its walls and hang pictures on the air.'
 ];
 
 //const text = quotes[Math.floor(Math.random() * quotes.length)];
